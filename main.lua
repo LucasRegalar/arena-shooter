77bf1fb41df0f149e5eaa83def468490cd70e33a
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-03-08 15:21:55 +0100

    add player speed

--- main.lua
+++ main.lua
@@ -2,20 +2,21 @@ function love.load()
 	Player = {}
 	Player.x = 300
 	Player.y = 300
+	Player.speed = 3
 end
 
 function love.update()
 	if love.keyboard.isDown("o") then
-		Player.x = Player.x + 1
+		Player.x = Player.x + Player.speed
 	end
 	if love.keyboard.isDown("i") then
-		Player.y = Player.y - 1
+		Player.y = Player.y - Player.speed
 	end
 	if love.keyboard.isDown("u") then
-		Player.y = Player.y + 1
+		Player.y = Player.y + Player.speed
 	end
 	if love.keyboard.isDown("y") then
-		Player.x = Player.x - 1
+		Player.x = Player.x - Player.speed
 	end
 end
 
