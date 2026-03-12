33bcad816dfc0b05f18bfd1d17bbdd668bed171f
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-03-12 19:44:30 +0100

    test parrot sprite

--- main.lua
+++ main.lua
@@ -3,23 +3,29 @@ function love.load()
 	Player.x = 300
 	Player.y = 300
 	Player.speed = 3
+	Player.sprite = love.graphics.newImage('sprites/parrot.png')
+
+	Background = {}
+	Background.sprite = love.graphics.newImage('sprites/background.png')
 end
 
 function love.update()
-	if love.keyboard.isDown("o") then
+	if love.keyboard.isDown("\\") then
 		Player.x = Player.x + Player.speed
 	end
-	if love.keyboard.isDown("i") then
+	if love.keyboard.isDown("0") then
 		Player.y = Player.y - Player.speed
 	end
-	if love.keyboard.isDown("u") then
+	if love.keyboard.isDown("p") then
 		Player.y = Player.y + Player.speed
 	end
-	if love.keyboard.isDown("y") then
+	if love.keyboard.isDown("o") then
 		Player.x = Player.x - Player.speed
 	end
 end
 
 function love.draw()
-	love.graphics.circle('fill', Player.x, Player.y, 30)
+	-- love.graphics.draw(Background.sprite, 0, 0)
+	-- love.graphics.draw(Player.sprite, Player.x, Player.y)
+	love.graphics.circle('fill', Player.x, Player.y, 10)
 end
