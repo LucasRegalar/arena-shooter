e7c37e6b53072ba3f4c39b789488f73a1e8f0807
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-03-14 21:49:24 +0100

    create player class

---
+++ classes/player.lua
@@ -0,0 +1,66 @@
+Player = {}
+Player.__index = Player
+
+function Player:new(x, y)
+	local self = setmetatable({}, Player)
+	self.x = x or 300
+	self.y = y or 300
+	self.speed = 3
+	self.scale = 3
+
+	self.spriteSheet = love.graphics.newImage('sprites/NuclearLeak_CharacterAnim_1.2/character_20x20_pink.png')
+	self.idleQuads = {}
+	self.idleFrame = 1
+	self.idleFrameTime = 0.15
+	self.idleTimer = 0
+
+	local spriteSheetWidth, spriteSheetHeight = self.spriteSheet:getDimensions()
+	-- -2 becuase idle animation has less quads then full spriteSheet length
+	for i = 0, (spriteSheetWidth / 20) - 1 - 2 do
+		self.idleQuads[i + 1] = love.graphics.newQuad(i * 20, 20, 20, 20, spriteSheetWidth, spriteSheetHeight)
+	end
+
+	return self
+end
+
+function Player:update(dt)
+	self:handleMovement()
+	self:updateAnimation(dt)
+end
+
+function Player:draw()
+	love.graphics.draw(
+		self.spriteSheet,
+		self.idleQuads[self.idleFrame],
+		self.x - 20,
+		self.y - 20,
+		0,
+		self.scale,
+		self.scale
+	)
+end
+
+function Player:handleMovement()
+	if love.keyboard.isDown("\\") then
+		self.x = self.x + self.speed
+	end
+	if love.keyboard.isDown("0") then
+		self.y = self.y - self.speed
+	end
+	if love.keyboard.isDown("p") then
+		self.y = self.y + self.speed
+	end
+	if love.keyboard.isDown("o") then
+		self.x = self.x - self.speed
+	end
+end
+
+function Player:updateAnimation(dt)
+	self.idleTimer = self.idleTimer + dt
+	if self.idleTimer >= self.idleFrameTime then
+		self.idleTimer = self.idleTimer - self.idleFrameTime
+		self.idleFrame = self.idleFrame % #self.idleQuads + 1
+	end
+end
+
+return Player
--- main.lua
+++ main.lua
@@ -1,50 +1,16 @@
+local Player = require("classes.player")
+local player
+
 function love.load()
 	-- important for pixel art
 	love.graphics.setDefaultFilter("nearest", "nearest")
-	Player = {}
-	Player.x = 300
-	Player.y = 300
-	Player.speed = 3
-	Player.sprite = love.graphics.newImage('sprites/parrot.png')
-	Player.idleSpriteSheet = love.graphics.newImage('sprites/NuclearLeak_CharacterAnim_1.2/character_20x20_pink.png')
-	Player.idleQuads = {}
-	Player.idleFrame = 1
-	Player.idleFrameTime = 0.15
-	Player.idleTimer = 0
-	Player.scale = 4
-
-	local spriteSheetWidth, spriteSheetHeight = Player.idleSpriteSheet:getDimensions()
-	for i = 0, (spriteSheetWidth / 20) - 1 - 2 do
-		Player.idleQuads[i + 1] = love.graphics.newQuad(i * 20, 20, 20, 20, spriteSheetWidth, spriteSheetHeight)
-	end
-
-	Background = {}
-	Background.sprite = love.graphics.newImage('sprites/background.png')
+	player = Player:new(300, 300)
 end
 
 function love.update(dt)
-	Player.idleTimer = Player.idleTimer + dt
-	if Player.idleTimer >= Player.idleFrameTime then
-		Player.idleTimer = Player.idleTimer - Player.idleFrameTime
-		Player.idleFrame = Player.idleFrame % #Player.idleQuads + 1
-	end
-
-	if love.keyboard.isDown("\\") then
-		Player.x = Player.x + Player.speed
-	end
-	if love.keyboard.isDown("0") then
-		Player.y = Player.y - Player.speed
-	end
-	if love.keyboard.isDown("p") then
-		Player.y = Player.y + Player.speed
-	end
-	if love.keyboard.isDown("o") then
-		Player.x = Player.x - Player.speed
-	end
+	player:update(dt)
 end
 
 function love.draw()
-	-- love.graphics.draw(Background.sprite, 0, 0)
-	-- love.graphics.draw(Player.sprite, Player.x, Player.y)
-	love.graphics.draw(Player.idleSpriteSheet, Player.idleQuads[Player.idleFrame], Player.x - 20, Player.y - 20, 0, Player.scale, Player.scale)
+	player:draw()
 end
