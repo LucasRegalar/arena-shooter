6d92f9169d420cd4ce16c67261f9982dd2143b47
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-03-22 10:45:58 +0100

    strip most of the stuff I don't understand

--- conf.lua
+++
@@ -1,8 +0,0 @@
---- Love2D configuration — runs before the window is created.
--- Sets initial window properties that must be defined before love.load().
-function love.conf(t)
-	t.window.title = "Love Love Love"
-	t.window.fullscreen = true
-	t.window.fullscreentype = "desktop"
-	t.window.icon = "assets/images/icon.png"
-end
--- main.lua
+++ main.lua
@@ -1,20 +1,8 @@
-Object = require "lib.classic"
-
-local Game = require("classes.game.init")
-local GameRenderer = require("classes.ui.gameRenderer")
-local game
-local gameRenderer
-
 function love.load()
-	game = Game()
-	gameRenderer = GameRenderer(game)
 end
 
 function love.update(dt)
-	game:update(dt)
-	gameRenderer:update(dt)
 end
 
 function love.draw()
-	gameRenderer:draw()
 end
